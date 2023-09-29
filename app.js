fd69044e50c47637477ea182ad055a9fa965356b
Vijay <viijay1999@gmail.com>
2023-09-29 20:49:05 +0530

    Update ejs files and app.js

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Welcome to my Daily Journal, your ultimate destination for journaling with a twist! 🚀 Here, you can embark on your personal journey of self-expression and reflection by crafting beautiful journals filled with your thoughts, experiences, and emotions. 🤗 Our user-friendly platform makes it a breeze to compose and publish your daily musings. Whether you're chronicling your adventures, recording your thoughts, or simply celebrating the little joys in life, Daily Journal is your canvas to create and share the stories that define you. 🌟 Join our community of fellow journal enthusiasts and embark on a meaningful voyage of self-discovery.📝✨ #DailyJournal #WriteYourStory";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -49,7 +47,7 @@ app.get("/", function(req, res){
       } else {
         res.render("home", {
           startingContent: homeStartingContent,
-          posts: posts
+          posts: foundPosts
           });
       }
     })
@@ -59,11 +57,11 @@ app.get("/", function(req, res){
 });
 
 app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+  res.render("about");
 });
 
 app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+  res.render("contact");
 });
 
 app.get("/compose", function(req, res){
