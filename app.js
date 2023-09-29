cb183e842f99ee3f9bf2b26b7dd58367eb2829ba
Vijay <viijay1999@gmail.com>
2023-09-29 19:17:15 +0530

    initial commit

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+
+/* 
+Mongoose connection to blogDB database running locally on the default port (27017). 
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+*/
+
+//Mongoose connection to MongoDB blogDB database
 mongoose.connect(process.env.MONGO_URL);
 
 app.set('view engine', 'ejs');
